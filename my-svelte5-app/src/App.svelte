91db37295a226e4186620824d6e0875e1972a619
Lara Ismael <lara8ismael@gmail.com>
2025-08-17 11:49:47 -0700

    learnign about  and

--- my-svelte5-app/src/App.svelte
+++ my-svelte5-app/src/App.svelte
@@ -1,11 +1,23 @@
 <script>
+  import Child from "./Child.svelte";
+
   let count = $state(0)
 </script>
 
 <main>
-  <h1>Hello Svelte 5!</h1>
-  <button onclick={() => count++}>Count: {count}</button>
+  <div class="container">
+    <h1>Learning Svelte 5</h1>
+  </div>
+  <div class="container">
+    <button onclick={() => count++}>Count: {count}</button>
+  </div>
+  <div class="container">
+    <Child message="Hello from Parent!"/>
+  </div>
 </main>
 
 <style>
+  .container {
+    margin-bottom: 15px;
+  }
 </style>
