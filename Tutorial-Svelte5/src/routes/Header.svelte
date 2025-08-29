cb81acba3f223bb93eacaf3f67520cee67f5e40e
Lara Ismael <lara8ismael@gmail.com>
2025-08-29 11:32:15 -0700

    understanding style :global and {#if} {:else if} {/if} statements

--- Tutorial-Svelte5/src/routes/Header.svelte
+++ Tutorial-Svelte5/src/routes/Header.svelte
@@ -1,16 +1,35 @@
 <script lang="ts">
   let { 
     name,
+    error,
     optional_name = "Leo" 
   }: { 
     name: string,
+    error: string,
     optional_name?: string 
   } = $props(); 
 
   // optional_name the ? tells TS that the value is optional so if you go to +page.svelte the implementation of <Header /> won't show an error eventhough we are not passing a value as optional_name. I'm setting a default value for it ont eh $props() object but that is also optional.
 </script>
 
+<div>
+  <h3>{optional_name}</h3>
+  <h1>{name}'s Form'</h1>
+</div>
+<h2 class="test">{name.replaceAll('a', 'X')}</h2>
+<h2 class="error">{error}</h2>
 
-<h3>{optional_name}</h3>
-<h1>Hello from {name}</h1>
-<h2>{name.replaceAll('a', 'X')}</h2>
+<style>
+  /* this style is unique to this file  if you have another file with a div or a class test this won't be passed. You can still create global css variables by adding a :global before the style block as shown below.*/
+  div {
+    background: green;
+  }
+
+  :global(.error) {
+    color: red;
+  }
+
+  .test {
+    background: blue;
+  }
+</style>
